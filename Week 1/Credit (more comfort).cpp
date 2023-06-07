4db9769d2ef821c6dc5bf2deffe898bc04be69be
Emmanuel <132662856+Emmanuel2-uni@users.noreply.github.com>
2023-06-07 19:54:37 +0800

    Add files via upload

---
+++ Week 1/Credit (more comfort).cpp
@@ -0,0 +1,231 @@
+#include <iostream>
+#include <cmath>
+// Testing CS50x style convention\
+//  https://cs50.readthedocs.io/style/c/
+// My rudimentary solution to their credit card exercise but in c++
+//  https://cs50.harvard.edu/x/2023/psets/1/credit/
+
+
+void get_card();
+int get_checksum();
+std::string check_validity(int checksum);
+std::string get_card_issuer();
+
+long long card_number;
+long long digit;
+bool run = true;
+int choice = 0;
+
+
+int main()
+{
+	do{
+		system("CLS");
+		int checksum = 0;
+	
+		get_card();
+		
+		std::cout << "\nCard number: " << card_number;
+		//std::cout << "\n\nCHECKSUM: " << get_checksum();
+	
+		checksum = get_checksum();
+	
+		std::cout << "\nCard Validity: " << check_validity(checksum);
+		std::cout << "\nIssuer: " << get_card_issuer();
+		std::cout << "\n\nCheck a different card? [1-Yes | 2-No] ";
+		std::cin >> choice;
+			if (choice==2){
+				run=false;
+			}
+	}while(run==true);
+	
+	return 0;
+}
+
+
+void get_card()
+{
+	std::cout << "Input card number: ";
+	std::cin >> card_number;	
+	
+}
+
+int get_checksum()
+{
+	long long remainder;
+	long long digit_place = 0;
+	long long card_number_backup = card_number;
+	int checksum_product = 0;
+	int checksum_noproduct = 0;
+	int checksum;
+	
+	int digit_product;
+	
+	do
+	{
+		digit_place++;
+		
+		// Using the modulo operation to determine the last digit.
+		digit = card_number_backup % (10);
+		
+		// Debug line below for following the digit place.
+		// std::cout << "\n\nDigit " << digit_place << " :" << digit;
+		
+		
+		// Read from right to left, where the rightmost digit is the first digit; using modulo operation to determine if the digit's place is even.
+		if ((digit_place%2)==0)
+		{
+			digit_product = digit*2;
+			
+			// std::cout << "\nCurrent digit product: " << digit_product;
+			
+			do
+			{	
+				checksum_product += (digit_product % 10);
+				
+				// Remove comment from the cout below to debug checksum products;
+				// std::cout << "\nAdding " << digit_product % 10 << " from " << digit_product;
+				// std::cout << "\nCurrent Checksum: " << checksum_product;
+				
+				digit_product = digit_product / 10;
+				
+			}while(digit_product!=0);
+			
+			
+		}
+		else if ((digit_place%2)!=0)
+		{
+			// Debug line below for checking which digit is being called by the if condition.
+			// std::cout << "\nDigit: " << digit;
+			
+			checksum_noproduct += digit;
+			
+		}
+	
+		// Using the integer data type and dividing by 10, we can cut the last digit out without any floating remainders.
+		remainder = card_number_backup / 10;
+		
+		// std::cout << "\n  Remaining digits: " << remainder;
+		
+		card_number_backup = remainder;
+		
+	}
+	while(card_number_backup!=0);
+	
+	checksum = checksum_product + checksum_noproduct;
+	
+	return checksum;
+	
+}
+
+
+std::string get_card_issuer()
+{
+	// American Express uses 15-digit numbers (starts with 34 or 37) 
+	// MasterCard uses 16-digit numbers (starts with 51 to 55) 
+	// And Visa uses 13- and 16-digit numbers (starts with 4)
+	
+	
+	long long card_number_backup = card_number;
+	long long remainder = card_number;
+	int digit_count=0;
+	int first_digit=0;
+	int second_digit=0;
+	std::string issuer;
+	
+	do
+	{
+		// Determines the number of digits that the card has.
+		digit_count++;
+		remainder = remainder / 10;
+	}
+	while(remainder!=0);
+	
+	
+	switch (digit_count)
+	{
+		case 16:
+			// Determine last digit if there are 16 numbers on the card
+			first_digit = card_number_backup / pow(10,15);
+			second_digit = (card_number_backup / pow(10,14));
+			second_digit = second_digit%10;
+			
+			if ( first_digit == 4 )
+			{
+				issuer = "VISA";
+			}
+			else if ( first_digit == 5 && (second_digit >= 1 || second_digit <= 5) )
+			{
+				issuer = "MasterCard";
+			}
+			else
+			{
+				issuer = "Invalid card number.";
+			}
+			
+			// Debug lines below for checking first and second digit;
+			// std::cout << "\nFirst digit: " << first_digit;
+			// std::cout << "\nSecond digit: " << second_digit;
+			
+			break;
+			
+			
+		case 13:
+			first_digit = card_number_backup / pow(10,12);
+			
+			if ( first_digit == 4 )
+			{
+				issuer = "VISA";
+			}
+			else
+			{
+				issuer = "Invalid card number.";
+			}		
+				
+			break;
+		
+		//first_digit == 5 && (second_digit <= 5 || second_digit >= 1)
+		
+		case 15:
+			first_digit = card_number_backup / pow(10,14);
+			second_digit = (card_number_backup / pow(10,13));
+			second_digit = second_digit%10;
+			
+			if ( first_digit == 3 && (second_digit == 4 || second_digit == 7) )
+			{
+				issuer = "American Express";
+			}
+			else
+			{
+				issuer = "Invalid card number.";
+			}
+			break;
+			
+			
+		default:
+			issuer = "Invalid card number.";
+			break;
+			
+			
+	}
+	
+	return issuer;
+	
+}
+
+
+std::string check_validity(int checksum)
+{
+	std::string validity;
+	
+	if ( checksum % 10 == 0 )
+	{
+		validity = "Valid.";
+	}
+	else if ( checksum % 10 != 0 )
+	{
+		validity = "Invalid.";
+	}
+	
+	return validity;
+}
